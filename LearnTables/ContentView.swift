3c1e8254db14829b926614cd55db8d37b29ef789
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-29 12:15:47 +0530

    Created the basic layout

--- LearnTables/ContentView.swift
+++ LearnTables/ContentView.swift
@@ -1,21 +1,35 @@
-//
-//  ContentView.swift
-//  LearnTables
-//
-//  Created by Manik Lakhanpal on 29/06/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstNumber = Int.random(in: 2...12)
+    @State private var secondNumber = Int.random(in: 2...12)
+
+    private var answer: Int {
+        return firstNumber * secondNumber
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.purple, .pink, .blue, .cyan, .green, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack {
+                Text("\(firstNumber) x ? = \(answer)")
+                    .font(.title)
+                    .foregroundStyle(.yellow)
+                    .fontDesign(.rounded)
+                    .fontWeight(.black)
+                
+                VStack {
+                    Button("1") {}
+                        .wid
+                    Button("2") {}
+                    Button("3") {}
+                }
+                .frame(maxWidth: .infinity)
+                .background(.ultraThinMaterial)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
