5d1d0c46bf5874c7fed996874987770f40414014
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-29 11:29:00 +0530

    Initial Commit

---
+++ LearnTables/LearnTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTablesApp.swift
+//  LearnTables
+//
+//  Created by Manik Lakhanpal on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
